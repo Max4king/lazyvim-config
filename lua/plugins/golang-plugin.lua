384a04c0892f934cd20cab6ba842886272afb13b
Max4king <ryan3311@hotmail.com>
2025-11-24 08:41:23 +0700

    initial lazyvim config

---
+++ lua/plugins/golang-plugin.lua
@@ -0,0 +1,6 @@
+return {
+  -- "fredrikaverpil/neotest-golang", -- Unit test coverage helper
+  "stevearc/conform.nvim", -- Formatter
+  -- "nvimtools/none-ls.nvim", -- A extension lib for lsp to allow more control via lua
+  "mfussenegger/nvim-lint", -- Asynchronous linter as a complementary for built-in LSP support
+}
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          win = {
+            list = {
+              keys = {
+                -- Map 'o' to 'confirm', which opens the file in the current buffer
+                ["o"] = "confirm",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
